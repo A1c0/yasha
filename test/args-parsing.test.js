22b94d044113f8dd680ef268273d5acf751c6dd2
Alexandre CONTASSOT <alexandrecontassot@hotmail.fr>
2023-07-25 16:55:29 +0200

    yop

---
+++ test/args-parsing.test.js
@@ -0,0 +1,64 @@
+import test from "oletus";
+import { parsedArgs } from "../lib/args-parsing.js";
+
+test("could parse command with one params", (t) => {
+  t.deepEqual(parsedArgs(["foo", "bar"]), { cmd: "foo", params: ["bar"] });
+  t.deepEqual(parsedArgs(["foo", "bar", "baz"]), {
+    cmd: "foo",
+    params: ["bar", "baz"],
+  });
+});
+
+test("could parse command with multiple params", (t) => {
+  t.deepEqual(parsedArgs(["foo", "bar", "baz"]), {
+    cmd: "foo",
+    params: ["bar", "baz"],
+  });
+
+  t.deepEqual(parsedArgs(["foo", "bar", "baz", "qux"]), {
+    cmd: "foo",
+    params: ["bar", "baz", "qux"],
+  });
+});
+
+test("could parse command with params and options", (t) => {
+  t.deepEqual(parsedArgs(["foo", "bar", "--baz", "bab"]), {
+    cmd: "foo",
+    params: ["bar"],
+    options: { baz: "bab" },
+  });
+
+  t.deepEqual(parsedArgs(["foo", "bar", "--baz", "bab", "--qux", "quux"]), {
+    cmd: "foo",
+    params: ["bar"],
+    options: { baz: "bab", qux: "quux" },
+  });
+
+  t.deepEqual(parsedArgs(["foo", "bar", "--baz", "bab", "--qux=quux"]), {
+    cmd: "foo",
+    params: ["bar"],
+    options: { baz: "bab", qux: "quux" },
+  });
+});
+
+test("could parse command with params and options", (t) => {
+  t.deepEqual(parsedArgs(["foo", "--help"]), {
+    cmd: "foo",
+    options: { help: true },
+  });
+
+  t.deepEqual(
+    parsedArgs(["foo", "bar", "--baz", "bab", "--joe", "--qux", "quux"]),
+    {
+      cmd: "foo",
+      params: ["bar"],
+      options: { baz: "bab", qux: "quux", joe: true },
+    },
+  );
+
+  t.deepEqual(parsedArgs(["foo", "bar", "--baz", "bab", "--qux=quux"]), {
+    cmd: "foo",
+    params: ["bar"],
+    options: { baz: "bab", qux: "quux" },
+  });
+});
---
+++ yasha.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+import { runListenYashaServer } from "./lib/server.js";
+
+// const args = parsedArgs(process.argv.slice(2));
+
+const stopServer = runListenYashaServer("http://localhost:8080", 8081, {
+  name: "test",
+  outputDir: "reports",
+});
+
+process.on("SIGINT", () => {
+  console.log("SIGINT received");
+  stopServer();
+  process.exit(0);
+});
